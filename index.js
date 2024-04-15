1eb75e2a9891c5e97b503b154cc0279660b57de5
Mohammedadn <mohammedaden023@gmail.com>
2024-04-15 12:01:21 +0300

    firt

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const newHeader= document.createElement('h1')
+newHeader.id= "victory"
+newHeader.textContent="Mohammed is the champion"
+
+// const element = document.getElementById('main');
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+// element.textContent =  "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+// element.className = "pet-listing dog";
+// element.className.remove ("dog")
+// element.className.add("cat","sale");
+// const ul = document.getElementsByTagName("ul")[0];
+// const secondChild = ul.querySelector("li:nth-child(2)");
+// ul.removeChild(secondChild);
